a6f0c523b63ab3537ef75350c275942d0674c4d0
yuka <youxiangyuanye@gmail.com>
2024-12-31 07:48:43 +0000

    AdminProfileController.php

--- app/Http/Controllers/AdminProfileController.php
+++ app/Http/Controllers/AdminProfileController.php
@@ -19,6 +19,27 @@ class AdminProfileController extends Controller
     {
         return view('admin.profile.edit');
     }   
+    use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\Admin\AAAController;
+Route::controller(AAAController::class)->group(function() {
+    Route::get('XXX', 'bbb');
+});
     public function update()
     {
         return redirect('admin/profile/edit');
