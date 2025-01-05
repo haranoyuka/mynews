dec931ba57343646f5654a3ff2dc14fe98dcb0d2
yuka <youxiangyuanye@gmail.com>
2025-01-05 13:13:43 +0000

    kadai10

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,16 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール編集画面')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>Myプロフィール編集</h2>
+        </div>
+    </div>
+</div>
